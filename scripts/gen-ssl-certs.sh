24534127106ecfa2e12f5a3d1bde660e6f5bf339
Andrew Dunn <andrew.dunn@melbourne.aus>
2017-01-12 01:47:37 +1100

    Removed PKCS#12 generation

--- scripts/gen-ssl-certs.sh
+++ scripts/gen-ssl-certs.sh
@@ -7,8 +7,8 @@ fi
 pushd config
 openssl genrsa -out key.pem 2048
 openssl req -new -sha256 -key key.pem -out csr.csr
-openssl req -x509 -sha256 -days 30 -key key.pem -in csr.csr -out certificate.pem
-openssl pkcs12 -export -out cert.pfx -inkey key.pem -in certificate.pem
+openssl req -x509 -sha256 -days 30 -key key.pem -in csr.csr -out cert.pem
+rm csr.csr
 cat > ssl.json << EOF
 {
   "selfSigned": true,
