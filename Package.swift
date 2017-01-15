bdd7bf3ef70c2c9bcd8cb7f43178d1ee3d031ef7
Andrew Dunn <andrew.dunn@melbourne.aus>
2017-01-15 23:00:35 +1100

    Added Kitura-Request as a dependency.

--- Package.swift
+++ Package.swift
@@ -8,5 +8,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0)
     ]
 )
