c97968148deb9ae43238b2e7c9cb45efa80a9b8b
Andrew Dunn <andrew.dunn@melbourne.aus>
2017-01-15 23:03:36 +1100

    Made slack command hide user's comment

--- Sources/shorts-salad/MainController.swift
+++ Sources/shorts-salad/MainController.swift
@@ -4,20 +4,45 @@
 
 import Foundation
 import Kitura
+import KituraRequest
 
 class MainController {
     let router: Router
 
     var port: Int {
-        get { return 8091 }
+        get {
+            return 8091
+        }
     }
 
     var url: String {
-        get { return "0.0.0.0" }
+        get {
+            return "0.0.0.0"
+        }
     }
 
     init() throws {
         router = Router()
-        router.all("/", middleware: StaticFileServer())
+        router.get("/") { request, response, next in
+            try response.send("Hello World, from Kitura!").end()
+        }
+        router.post("/slack/show-salad-link") { request, response, next in
+            try response.send("").end()
+
+            let responseURL = request.body?.asJSON?["response_url"].string
+            if responseURL != nil {
+                let message = "<https://\(request.hostname)|:middle_finger::jeans:.ws>"
+                KituraRequest.request(.post,
+                                      responseURL!,
+                                      parameters: [
+                                          "response_type": "in_channel",
+                                          "attachments": [
+                                              "fallback": message,
+                                              "pretext": message
+                                          ]
+                                      ],
+                                      encoding: JSONEncoding.default)
+            }
+        }
     }
 }
